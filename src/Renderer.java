e5a2f0bf4c9afb5ab7bcdea580b09cb6be98be76
Michael Ganzorig <manlai1025@gmail.com>
2019-03-31 21:51:52 -0700

    changing coordinate system zero

--- src/Renderer.java
+++ src/Renderer.java
@@ -3,6 +3,7 @@ package src;
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
 import src.*;
@@ -47,9 +48,11 @@ public final class Renderer
 
     private void fillTriangle(Vector3 p1, Vector3 p2, Vector3 p3, Color color)
     {
-        Graphics g = drawBuffer.getDrawGraphics();
+        Graphics2D g = (Graphics2D) drawBuffer.getDrawGraphics();
         if(g == null)   return;
         g.setColor(color);
+        g.translate(0, height);
+        g.scale(1, -1);
 
         int smallestX = (int) Math.min(p1.x, Math.min(p2.x, p3.x));
         int smallestY = (int) Math.min(p1.y, Math.min(p2.y, p3.y));
@@ -70,9 +73,11 @@ public final class Renderer
 
     private void fillTriangleZBuffer(Vector3 p1, Vector3 p2, Vector3 p3, Color color, float[][] buffer)
     {
-        Graphics g = drawBuffer.getDrawGraphics();
+        Graphics2D g = (Graphics2D) drawBuffer.getDrawGraphics();
         if(g == null)   return;
         g.setColor(color);
+        g.translate(0, height);
+        g.scale(1, -1);
 
         int smallestX = (int) Math.min(p1.x, Math.min(p2.x, p3.x));
         int smallestY = (int) Math.min(p1.y, Math.min(p2.y, p3.y));
@@ -100,7 +105,7 @@ public final class Renderer
     }
 
     double rotationDelta = 0;
-    int incRate = 4;
+    int incRate = 2;
     public void renderLightedZBuffer(Mesh mesh, Camera cam, float scale, Light light)
     {
         float[][] buffer = new float[width][height];
@@ -114,15 +119,8 @@ public final class Renderer
         Vector3 camPos = cam.getPosition();
         Vector3 meshPos = mesh.transform.getPosition();
 
-        Matrix4x4 translate = new Matrix4x4(new Vector3(meshPos.x + camPos.x,
-                                    meshPos.y + camPos.y, meshPos.z + camPos.z));
+        Matrix4x4 translate = new Matrix4x4(Common.addVectors(meshPos, camPos));
         translate.scale(scale);
-
-        /*Matrix4x4 rotateZ = new Matrix4x4();
-        rotateZ.m[0][0] = (float) Math.cos(Math.PI);
-        rotateZ.m[1][0] = (float) -Math.sin(Math.PI);
-        rotateZ.m[0][1] = (float) Math.sin(Math.PI);
-        rotateZ.m[1][1] = (float) Math.cos(Math.PI);*/
 
         for(int i = 0; i < tris.size(); i += 3)
         {
@@ -130,17 +128,6 @@ public final class Renderer
             Vector3 p1 = verts.get(tris.get(i) - 1);
             Vector3 p2 = verts.get(tris.get(i+1) - 1);
             Vector3 p3 = verts.get(tris.get(i+2) - 1);
-
-            // applying transformation and scaling on the vertex
-            /*Vector3 p1New = new Vector3(p1.x * scale,
-                        height - p1.y * scale - meshPos.y + camPos.y,
-                        p1.z * scale);
-            Vector3 p2New = new Vector3(p2.x * scale,
-                        height - p2.y * scale - meshPos.y + camPos.y,
-                        p2.z * scale);
-            Vector3 p3New = new Vector3(p3.x * scale,
-                        height - p3.y * scale - meshPos.y + camPos.y,
-                        p3.z * scale);*/
 
             double delta = Math.toRadians(rotationDelta);
 
@@ -150,7 +137,7 @@ public final class Renderer
             rotateY.m[0][2] = (float) Math.sin(delta);
             rotateY.m[2][2] = (float) Math.cos(delta);
 
-            Matrix4x4 translateAfterRot = new Matrix4x4(new Vector3(width * (float)Math.sin(delta/2), 0));          
+            Matrix4x4 translateAfterRot = new Matrix4x4(new Vector3(width * (float)Math.sin(delta/2), 0));      
 
             Matrix4x4 p1Mat = new Matrix4x4(p1);
             p1Mat.apply(translate).apply(rotateY).apply(translateAfterRot);
@@ -164,53 +151,36 @@ public final class Renderer
             p3Mat.apply(translate).apply(rotateY).apply(translateAfterRot);
             Vector3 p3New = p3Mat.getPosition();
 
-            /*
-            float x = p1New.x;
-            float z = p1New.z;
-            x = (float)Math.cos(delta) * x + (float)Math.sin(delta) * z;
-            z = (float)-Math.sin(delta) * x + (float)Math.cos(delta) * z;
-            p1New.x = x + width * (float)Math.sin(delta/2);
-            p1New.z = z + width * (float)Math.sin(delta/2);
-
-            float x2 = p2New.x;
-            float z2 = p2New.z;
-            x2 = (float)Math.cos(delta) * x2 + (float)Math.sin(delta) * z2;
-            z2 = (float)-Math.sin(delta) * x2 + (float)Math.cos(delta) * z2;
-            p2New.x = x2 + width * (float)Math.sin(delta/2);
-            p2New.z = z2 + width * (float)Math.sin(delta/2);
-
-            float x3 = p3New.x;
-            float z3 = p3New.z;
-            x3 = (float)Math.cos(delta) * x3 + (float)Math.sin(delta) * z3;
-            z3 = (float)-Math.sin(delta) * x3 + (float)Math.cos(delta) * z3;
-            p3New.x = x3 + width * (float)Math.sin(delta/2);
-            p3New.z = z3 + width * (float)Math.sin(delta/2);
-            */
-
             Vector3 normal = Common.crossProduct(Common.vectorFromVector3(p1New, p2New),
                                             Common.vectorFromVector3(p1New, p3New));
             float intensity = light.getIntensity(normal);
             
             // minimum and maximum grayscale value
-            float minimumIntensity = 0f;
+            float minimumIntensity = 10f;
             int grayscale = (int) Math.max(minimumIntensity, 255 * intensity);
             fillTriangleZBuffer(p1New, p2New, p3New, new Color(grayscale, grayscale, grayscale), buffer);
         }
         rotationDelta += incRate;
-        if(rotationDelta >= 360)
-            rotationDelta = 0;
+        if(rotationDelta >= 360 || rotationDelta <= 0)
+            incRate *= -1;
     }
 
     public void wireFrameRender(Mesh mesh, Camera cam, float scale, Color color)
     {
-        // TODO: handle rotation also
-
-        Graphics g = drawBuffer.getDrawGraphics();
+        Graphics2D g = (Graphics2D) drawBuffer.getDrawGraphics();
         if(g == null)   return;
         g.setColor(color);
+        g.translate(0, height);
+        g.scale(1, -1);
 
         ArrayList<Vector3> verts = mesh.getVertices();
         ArrayList<Integer> tris = mesh.getTriangles();
+
+        Vector3 camPos = cam.getPosition();
+        Vector3 meshPos = mesh.transform.getPosition();
+
+        Matrix4x4 translate = new Matrix4x4(Common.addVectors(meshPos, camPos));
+        translate.scale(scale);
 
         for(int i = 0; i < tris.size(); i += 3)
         {
@@ -219,23 +189,17 @@ public final class Renderer
             Vector3 p2 = verts.get(tris.get(i+1) - 1);
             Vector3 p3 = verts.get(tris.get(i+2) - 1);
 
-            Vector3 camPos = cam.getPosition();
+            Matrix4x4 p1Mat = new Matrix4x4(p1);
+            p1Mat.apply(translate);
 
-            Vector3 meshPos = mesh.transform.getPosition();
+            Matrix4x4 p2Mat = new Matrix4x4(p2);
+            p2Mat.apply(translate);
 
-            // applying transformation and scaling on the vertices
-            Vector3 p1New = new Vector3(p1.x * scale + meshPos.x + camPos.x,
-                        height - p1.y * scale - meshPos.y + camPos.y,
-                        p1.z * scale + meshPos.z + camPos.z);
-            Vector3 p2New = new Vector3(p2.x * scale + meshPos.x + camPos.x,
-                        height - p2.y * scale - meshPos.y + camPos.y,
-                        p2.z * scale + meshPos.z + camPos.z);
-            Vector3 p3New = new Vector3(p3.x * scale + meshPos.x + camPos.x,
-                        height - p3.y * scale - meshPos.y + camPos.y,
-                        p3.z * scale + meshPos.z + camPos.z);
+            Matrix4x4 p3Mat = new Matrix4x4(p3);
+            p3Mat.apply(translate);
 
             // drawing the triangle
-            triangle(p1New, p2New, p3New, g);
+            triangle(p1Mat.getPosition(), p2Mat.getPosition(), p3Mat.getPosition(), g);
         }
         g.dispose();
     }
