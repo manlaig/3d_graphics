a40f83507ff0de7679dfb177b39faa2f8c037bdc
Michael Ganzorig <manlai1025@gmail.com>
2019-03-31 18:51:19 -0700

    adding matrix operation functions

--- src/Common.java
+++ src/Common.java
@@ -51,4 +51,30 @@ public final class Common
         triangle is degenerate, in this case return something with negative coordinates */
         return new Vector3(1.f-(u.x-u.y)/u.z, -u.y/u.z, u.x/u.z); 
     }
+
+    public static Matrix4x4 matrixMultiply(Matrix4x4 left, Matrix4x4 right)
+    {
+        Matrix4x4 result = new Matrix4x4();
+
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 4; j++)
+            {
+                float sum = 0;
+                for(int k = 0; k < 4; k++)
+                    sum += left.m[i][k] * right.m[k][j];
+                result.m[i][j] = sum;
+            }
+
+        return result;
+    }
+
+    public static void printMatrix(Matrix4x4 mat)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+                System.out.print(mat.m[i][j] + " ");
+            System.out.println();
+        }
+    }
 }
