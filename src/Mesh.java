4a7f0acfa441ca83b726c305824c6b66f11be90a
Michael Ganzorig <manlai1025@gmail.com>
2019-04-15 16:59:08 -0700

    parsing texture data from obj file

--- src/Mesh.java
+++ src/Mesh.java
@@ -4,7 +4,7 @@ import src.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 import javax.swing.*;
-import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -15,7 +15,10 @@ public class Mesh extends SceneObject
 
     private ArrayList<Vector3> vertices;
     private ArrayList<Vector3> normals;
-    private ArrayList<Integer> triangles;
+    private ArrayList<Integer> vertexTriangles;
+    private ArrayList<Integer> textureTriangles;
+    private ArrayList<Vector3> textureCoordinates;
+    private BufferedImage texture;
 
     public Mesh(JFrame window, String filePath, Vector3 pos, float scale) throws FileNotFoundException
     {
@@ -23,14 +26,26 @@ public class Mesh extends SceneObject
         parseOBJFile(filePath);
     }
 
+    public Mesh(JFrame window, String filePath, Vector3 pos, float scale, BufferedImage img) throws FileNotFoundException
+    {
+        super(pos, scale);
+        parseOBJFile(filePath);
+        texture = img;
+    }
+
     public ArrayList<Vector3> getVertices()
     {
         return vertices;
     }
 
-    public ArrayList<Integer> getTriangles()
+    public ArrayList<Integer> getVertexTriangles()
     {
-        return triangles;
+        return vertexTriangles;
+    }
+
+    public ArrayList<Integer> getTextureTriangles()
+    {
+        return textureTriangles;
     }
 
     public ArrayList<Vector3> getNormals()
@@ -38,9 +53,24 @@ public class Mesh extends SceneObject
         return normals;
     }
 
+    public ArrayList<Vector3> getTextureCoords()
+    {
+        return textureCoordinates;
+    }
+
     public boolean vertexShadingSupported()
     {
         return canVertexShade;
+    }
+
+    public BufferedImage getTexture()
+    {
+        return texture;
+    }
+
+    public boolean isTextured()
+    {
+        return texture != null && !textureCoordinates.isEmpty();
     }
 
     // look at .obj file format before reading this function
@@ -48,7 +78,9 @@ public class Mesh extends SceneObject
     {
         normals = new ArrayList<>();
         vertices = new ArrayList<>();
-        triangles = new ArrayList<>();
+        vertexTriangles = new ArrayList<>();
+        textureTriangles = new ArrayList<>();
+        textureCoordinates = new ArrayList<>();
 
         // to normalize the positions of the meshes
         Vector3 smallest = new Vector3(1, 1, 1);
@@ -64,37 +96,35 @@ public class Mesh extends SceneObject
 
             if(nums[0].equals("v"))
             {
-                // vertex
-                try
-                {
-                    // nums[0] is "v"
-                    // vertices data starts from nums[1]
-                    float x = Float.parseFloat(nums[1]);
-                    float y = Float.parseFloat(nums[2]);
-                    float z = Float.parseFloat(nums[3]);
-                    
-                    if(x > smallest.x)  smallest.x = x;
-                    if(y > smallest.y)  smallest.y = y;
-                    if(z > smallest.z)  smallest.z = z;
+                // nums[0] is "v"
+                // vertices data starts from nums[1]
+                float x = Float.parseFloat(nums[1]);
+                float y = Float.parseFloat(nums[2]);
+                float z = Float.parseFloat(nums[3]);
+                
+                if(x > smallest.x)  smallest.x = x;
+                if(y > smallest.y)  smallest.y = y;
+                if(z > smallest.z)  smallest.z = z;
 
-                    /*
-                        the default origin is at top left,
-                        so the mesh is flipped on the z-axis
-                        fix: subtract from window height
-                    */
-                    Vector3 vertex = new Vector3(x, y, z);
-                    vertices.add(vertex);
-                }
-                catch(NumberFormatException e) {}
+                /*
+                    the default origin is at top left,
+                    so the mesh is flipped on the z-axis
+                    fix: subtract from window height
+                */
+                vertices.add(new Vector3(x, y, z));
+            }
+            else if (nums[0].equals("vt"))
+            {
+                float x = Float.parseFloat(nums[1]);
+                float y = Float.parseFloat(nums[2]);
+                textureCoordinates.add(new Vector3(x, y, 0));
             }
             else if (nums[0].equals("vn"))
             {
                 float x = Float.parseFloat(nums[1]);
                 float y = Float.parseFloat(nums[2]);
                 float z = Float.parseFloat(nums[3]);
-
-                Vector3 normal = new Vector3(x, y, z);
-                normals.add(normal);
+                normals.add(new Vector3(x, y, z));
             }
             else if (nums[0].equals("f"))
             {
@@ -105,9 +135,15 @@ public class Mesh extends SceneObject
                 String[] v2 = nums[3].split("/");
 
                 // vi[0] is the vertex that we want
-                triangles.add(Integer.valueOf(v0[0]));
-                triangles.add(Integer.valueOf(v1[0]));
-                triangles.add(Integer.valueOf(v2[0]));
+                vertexTriangles.add(Integer.valueOf(v0[0]));
+                vertexTriangles.add(Integer.valueOf(v1[0]));
+                vertexTriangles.add(Integer.valueOf(v2[0]));
+
+                // vi[1] is the index of the texture coordinate array
+                // we will use that value for map textures onto triangles
+                textureTriangles.add(Integer.valueOf(v0[1]));
+                textureTriangles.add(Integer.valueOf(v1[1]));
+                textureTriangles.add(Integer.valueOf(v2[1]));
             }
         }
         sc.close();
